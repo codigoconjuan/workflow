ba92ed65523976152e699089e551b4e5442111d1
JuanDevWP <blackmaxxgdl18@hotmail.com>
2016-12-15 23:11:57 -0600

    browser sync added

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,37 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
-
+var SOURCEPATHS = {
+  
+  sassSource : 'src/scss/*.scss'
+}
+var APPPATH ={
+  root: 'app/',
+  css : 'app/css',
+  js : 'app/js'
+}
 
 
 gulp.task('sass', function(){
-  return gulp.src('src/scss/app.scss')
+  return gulp.src(SOURCEPATHS.sassSource)
       .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-      .pipe(gulp.dest('app/css'));
+      .pipe(gulp.dest(APPPATH.css));
 });
 
-gulp.task('default', ['sass']);
+gulp.task('serve', ['sass'], function() {
+  browserSync.init([APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'], {
+    server: {
+      baseDir : APPPATH.root
+    }
+  })
+});
+
+gulp.task('watch', ['serve', 'sass'], function() {
+  gulp.watch([SOURCEPATHS.sassSource], ['sass']);
+} );
+
+gulp.task('default', ['watch']);
+
+
