8a36fc8abead8fabbbbb2c233ade66192fde855b
JuanDevWP <blackmaxxgdl18@hotmail.com>
2016-12-16 22:23:07 -0600

    bootstrap fonts added

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,8 @@ var SOURCEPATHS = {
 var APPPATH ={
   root: 'app/',
   css : 'app/css',
-  js : 'app/js'
+  js : 'app/js',
+  fonts: 'app/fonts'
 }
 
 gulp.task('clean-html', function() {
@@ -41,6 +42,11 @@ gulp.task('sass', function(){
           .pipe(gulp.dest(APPPATH.css));
 });
 
+gulp.task('moveFonts', function() {
+  gulp.src('./node_modules/bootstrap/dist/fonts/*.{eot,svg,ttf,woff,woff2}')
+      .pipe(gulp.dest(APPPATH.fonts));
+});
+
 gulp.task('scripts',['clean-scripts'],  function() {
   gulp.src(SOURCEPATHS.jsSource)
       .pipe(concat('main.js'))
@@ -61,7 +67,7 @@ gulp.task('serve', ['sass'], function() {
   })
 });
 
-gulp.task('watch', ['serve', 'sass', 'copy', 'clean-html', 'clean-scripts', 'scripts'], function() {
+gulp.task('watch', ['serve', 'sass', 'copy', 'clean-html', 'clean-scripts', 'scripts', 'moveFonts'], function() {
     gulp.watch([SOURCEPATHS.sassSource], ['sass']);
     gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
     gulp.watch([SOURCEPATHS.jsSource], ['scripts']);
