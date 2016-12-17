3959f0c9ee60d1b41bdd5fda628a9d0f2507d7a7
JuanDevWP <blackmaxxgdl18@hotmail.com>
2016-12-16 23:52:40 -0600

    readme edited

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# workflow
+# My Personal Workflow with SASS, NPM and Gulp
 My Web Workflow with NPM, SASS, Gulp and More
+
+#Instructions
+1.- Clone this repo or download it from this website.
+2.- Make sure you have these installed:
+    - [node.js](http://nodejs.org/)
+    - [git](http://git-scm.com/)
+    - [gulp](http://gulpjs.com/)
+3.- Run `npm install ` to install the project dependencies   
--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,7 @@ var injectPartials = require('gulp-inject-partials');
 var minify = require('gulp-minify');
 var rename = require('gulp-rename');
 var cssmin = require('gulp-cssmin');
+var htmlmin = require('gulp-htmlmin');
 
 var SOURCEPATHS = {
   sassSource : 'src/scss/*.scss',
@@ -92,6 +93,13 @@ gulp.task('compresscss', function(){
           .pipe(gulp.dest(APPPATH.css));
 });
 
+gulp.task('minifyHtml', function() {
+   return gulp.src(SOURCEPATHS.htmlSource)
+        .pipe(injectPartials())
+        .pipe(htmlmin({collapseWhitespace:true}))
+        .pipe(gulp.dest(APPPATH.root))
+});
+
 /** End of Production Tasks **/
 
 gulp.task('html', function() {
@@ -124,4 +132,5 @@ gulp.task('watch', ['serve', 'sass', 'clean-html', 'clean-scripts', 'scripts', '
 
 gulp.task('default', ['watch']);
 
+gulp.task('production', ['minifyHtml', 'compresscss', 'compress'] );
 
